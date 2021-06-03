dd3dd82c126eeab57e5b1902d9ea62e9ba317314
enicio <jessandro42@gmail.com>
2021-06-02 22:10:18 -0300

    Teste com button na tela cheia

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -1,52 +1,55 @@
 import React, { useState, useEffect } from 'react';
 import './dropdown.css';
 
-const Window = require('window');
-const window = new Window();
+// const Window = require('window');
+// const window = new Window();
 
 
 function DropDown() {
   const [ dropdown, setDropdown ] = useState(false);
 
-  useEffect(() => {
-    if(window.innerWidth > 400) setDropdown(true);
-  },[])
+  // useEffect(() => {
+  //   if(window.innerWidth > 400) setDropdown(true);
+  //   console.log(window.document.body)
+  // },[])
 
   function toggleDropdown() {
     setDropdown(!dropdown)
   }
-  let timeoutId = null;
 
-  function handleResize() {
-    clearTimeout(timeoutId);
+  // let timeoutId = null;
 
-    timeoutId = setTimeout(() => {
-      if(window.innerWidth > 400) setDropdown(true);
-      if(window.innerWidth < 400) setDropdown(false);
-    }, 200);
-  }
+  // function handleResize() {
+  //   clearTimeout(timeoutId);
 
-  window.addEventListener('resize', handleResize)
+  //   timeoutId = setTimeout(() => {
+  //     if(window.innerWidth > 400) setDropdown(true);
+  //     if(window.innerWidth < 400) setDropdown(false);
+  //   }, 200);
+  // }
+
+  // window.addEventListener('resize', handleResize);
+
   return(
     <div  className="dropdown">
-    <button
-      onClick={() => toggleDropdown()}
-      className="dropbtn">
-        <img className={(dropdown)
-          ? "dropdown__arrow__down"
-          : "dropdown__arrow__up"}
-          src="assets/arrow.svg"
-          alt="arrow"
-        />
-    </button>
-    {
-      (dropdown)
-      ?<div className="dropdown-content">
-         <a href="/">Change to teacher mode</a>
-       </div>
-      : ''
-    }
- </div>
+      <button
+        onClick={() => toggleDropdown()}
+        className="dropbtn">
+          <img className={(dropdown)
+            ? "dropdown__arrow__down"
+            : "dropdown__arrow__up"}
+            src="assets/arrow.svg"
+            alt="arrow"
+          />
+      </button>
+      {
+        (dropdown)
+        ?<div className="dropdown-content">
+          <a href="/">Change to teacher mode</a>
+        </div>
+        : <button className="dropdown-content-2" > Change to teacher mode </button>
+      }
+   </div>
   );
 }
 
